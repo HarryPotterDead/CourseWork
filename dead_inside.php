b4e272754c7cf1377df214e1f7afde6c18af3efa
HarryPotterDead <z4h3r228@gmail.com>
2021-02-26 16:40:23 +0300

    commit

--- dead_inside.php
+++ dead_inside.php
@@ -1,3 +1,6 @@
-<video width="640" height="360" autoplay="" loop controls >
+<div class="text">
+  <p>Рамзес иконе дединсайдов, выйди в окно</p>
+</div>
+<video width="75%" height="42%" autoplay="" loop controls >
  <source src="/Assets/Video/HarryPotterDead.mp4" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'>
 </video>
