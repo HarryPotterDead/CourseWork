592240715ecae0358bc24fab963d2ada7384d566
HarryPotterDead <z4h3r228@gmail.com>
2021-03-02 18:57:22 +0300

    commit

--- action.php
+++ action.php
@@ -1,15 +1,35 @@
 <?php
+if (isset($_GET['registration'])){
+    if (empty($_GET['login'])|| empty($_GET['pasw'])){
+        $message="Введите Логин и Пароль";
+    } else {
+        $r=$db->dbs->prepare('select * from users where login=:j;');
+        $r->execute([':j'=>$_GET['login']]);
+            if ($r->rowCount()!=0){
+                $message="Такой пользователь уже зарегестрирован";
+            } else {
+                $r=$db->dbs->prepare('insert into users set login=:j, pasw=:j1, `name`=:j2, email=:j3, phone=:j4;');
+                $r->execute([
+                    ':j'=>$_GET['login'],
+                    ':j1'=>md5($_GET['pasw']),
+                    ':j2'=>$_GET['name'],
+                    ':j3'=>$_GET['email'],
+                    ':j4'=>$_GET['phone']
+                ]);
+            }
+    }
 
-if (isset($_REQUEST['login'])){
-    if (($_REQUEST['login']!='') && ($_REQUEST['pasw']!='')){
-        $r=$db->dbs->prepare('SELECT * FROM users WHERE login=:i;');
-        $r->execute([':i'=>$_REQUEST['login']]);
-        if ($r->rowCount()!=0){
-            $message.="Duplicated login";
-        }else{
-            $pasw=md5($_REQUEST['pasw']);
-            $r=$db->dbs->prepare('INSERT INTO users SET login=:i, pasw=:i1');
-            ($r->execute([':i'=>$_REQUEST['login'], ':i1'=>$pasw]))?$message.="Inserting":$message.="Not Inserting";
-        }
-    }else $message.="Empty data";
+}
+
+if (isset($_REQUEST['auth'])) {
+$r=$db->dbs->prepare("SELECT * FROM users WHERE login=:j and pasw=:j1");
+$r->execute([':j'=>$_REQUEST['login'], ':j1'=>md5($_REQUEST['pasw'])]);
+if ($r->rowCount()!=0){
+    foreach($r as $res){
+        $_SESSION['test']="jhhjgh";
+        $_SESSION['login']=$res['login'];
+        $_SESSION['name']=$res['name'];
+        $message="Вы успешно авторизованы-".$r->rowCount();
+    }
+}else $message="error-".$r->rowCount();
 }
--- index.php
+++ index.php
@@ -16,10 +16,6 @@ require_once 'action.php';
 
     <link rel="stylesheet" href="css/MainPage/menu_up.css">
     <link rel="stylesheet" href="css/MainPage/Main.css">
-    <link rel="stylesheet" href="css/MainPage/Badrooms.css">
-    <link rel="stylesheet" href="css/MainPage/Livingrooms.css">
-    <link rel="stylesheet" href="css/MainPage/Kitchens.css">
-    <link rel="stylesheet" href="css/MainPage/AboutUs.css">
     <link rel="stylesheet" href="css/MainPage/side_menu.css">
 
     <link rel="stylesheet" href="css/MainPage/MainField.css">
@@ -45,22 +41,17 @@ require_once 'action.php';
             <li><a class="a_txt" href="index.php?page=contact">Контакты</a></li>
         </ul>
         <div class="Login">
-            <a class="a_txt" href="index.php?page=auth">Войти</a>
-        </div>
+            <?php if (isset($_REQUEST['login'])): ?>
 
-        <!-- Старое меню. Не трогать, вдруг понадобится
-        <div class="Main"> <a href="index.html" class="MainButton">Главная</a> </div>
-        <div class="Badrooms"> <a href="index.html" class="MainButton">Спальни</a> </div>
-        <div class="Livingrooms"> <a href="index.html" class="MainButton">Гостинные</a> </div>
-        <div class="Kitchens"> <a href="index.html" class="MainButton">Кухни</a> </div>
-        <div class="AboutUs"> <a href="index.html" class="MainButton">О Нас</a> </div>
-        -->
+                <a class="a_txt" href="index.php?page=auth"><? print($_REQUEST['login']) ?></a>
+
+                <?php else: ?>
+
+                <a class="a_txt" href="index.php?page=auth">Войти</a>
+
+                <?php endif ?>
+        </div>
     </div>
-<!--    <div class="side_menu">
-        <p>Боковая менюшка</p>
-        <p>Растягивается под , контент</p>
-        <p>держу в курсе</p>
-    </div> -->
     <div class="main_field">
 
 
